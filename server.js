3e449d6a906bf9d2849d8bbc4cfc4cbfa02f5e05
zibrahim2046 <zibrahim2046@gmail.com>
2020-04-20 13:33:28 -0500

    set up survey questions as well as results modal

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-// Dependencies
-var express = require("express");
+var express = require('express');
 var bodyParser = require("body-parser");
-var app = express();
 var path = require("path");
 
+// Sets up the Express app to handle data parsing
 
-// Set initial port 
-var PORT = process.env.PORT || 8080;
+var app = express();
+var PORT = 3000;
 
-// BodyParser to interpret data sent to server
+// Configure Express.js middle-wares
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Points server to CSS and JS static route files
-app.use(express.static(path.join(__dirname + '/app/public')));
-
-// Points server to route files
+//grab the routes
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
 
-// LISTENER: "starts" our server and listens for an event
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+// Starts the server to begin listening
+// =============================================================
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("App listening on PORT " + PORT);
 });
