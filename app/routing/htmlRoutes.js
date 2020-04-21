b210fe01e2aab623086f0558599d1d78feff6d2b
zibrahim2046 <zibrahim2046@gmail.com>
2020-04-21 10:21:17 -0500

    changed file path

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,9 +7,9 @@ var path = require("path");
 //we are going to declare a module exports object that will take the routes we will establish here and make them easily usable in the serverjs file.
 module.exports = function(app){
 	app.get('/',function(request, response){
-		response.sendFile(path.join(__dirname, '../home.html'));
+		response.sendFile(path.join(__dirname, '../public/home.html'));
 	});
 	app.get('/survey',function(request, response){
-		response.sendFile(path.join(__dirname,'../survey.html'));
+		response.sendFile(path.join(__dirname,'../public/survey.html'));
 	});
 }
